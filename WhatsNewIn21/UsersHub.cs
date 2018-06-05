58fefe26cf431aab325818ebc4326f4602747e30
Steve Smith <steve@kentsmiths.com>
2018-06-05 17:09:34 -0400

    Wiring up Register to toastr

--- WhatsNewIn21/UsersHub.cs
+++ WhatsNewIn21/UsersHub.cs
@@ -1,14 +1,8 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System.Threading.Tasks;
 
 namespace WhatsNewIn21
 {
-
     public class UsersHub : Hub
     {
-        public async Task SendSignIn(string userName)
-        {
-            await Clients.Others.SendAsync("ReceiveSignIn", userName);
-        }
     }
 }
--- WhatsNewIn21/wwwroot/js/site.js
+++ WhatsNewIn21/wwwroot/js/site.js
@@ -1,8 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your Javascript code.
-const userUpdatesConnection = new signalR.HubConnectionBuilder()
+﻿const userUpdatesConnection = new signalR.HubConnectionBuilder()
     .withUrl("/userUpdates")
     .build();
 
